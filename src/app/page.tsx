2fedae87fb16b4a740af345b120dec6320da2dcb
jorisvanandel <jorisvanandel@users.noreply.github.com>
2025-04-07 09:49:14 +0200

    feat: improve header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,10 +114,9 @@ export default function Home() {
           >
               <div className="flex-1">
                   <p className="text-muted-foreground [&>span]:text-foreground">
-                      I&apos;m Joris, a 26-year old <span>tech enthusiast</span> and <span>aspiring entrepreneur</span> based
-                      in <span>Amsterdam</span>. I am currently working as
-                      a <span>full-stack developer</span> at <span>PAQT</span>. I have a passion for crafting <span>elegant solutions</span> for
-                      complex problems.
+                      I&apos;m Joris, a 26-year old <span>tech enthusiast</span> and <span>solopreneur</span> 
+                      with a passion for crafting <span>elegant solutions</span> for complex problems. I am based in 
+                      Amsterdam and currently working as a <span>full-stack developer</span> at <span>PAQT</span>.
                   </p>
               </div>
           </motion.section>
